1cd26a7eccdf4280bdb35983c0b79c4c8d939d12
Andrew Player <andrewplayer3@gmail.com>
2024-08-07 23:20:29 -0500

    marginally cleaner

--- s1_level0/s1_level0_utils.py
+++ s1_level0/s1_level0_utils.py
@@ -32,63 +32,59 @@ def huffman_4(bit_string):
     m_code = 0
     bit, bit_string = read_and_pop(bit_string, 1)
     bit_len = 1
-    if bit == '0':  # 0 <-
+    if bit == '0':
         bit, bit_string = read_and_pop(bit_string, 1)
         bit_len += 1
-        if bit == '0':  # 0 <- 0 <-
-            return 0, bit_string, bit_len
-        else:
-            bit, bit_string = read_and_pop(bit_string, 1)
-            bit_len += 1
-            return (1, bit_string, bit_len) if bit == '0' else (2, bit_string, bit_len)  # 0 or 1 <- 1 <- 0
+        if bit == '1':
+            m_code = 1 if bit == '0' else 2
     else:
         bit, bit_string = read_and_pop(bit_string, 1)
         bit_len += 1
         if bit == '0':
             bit, bit_string = read_and_pop(bit_string, 1)
             bit_len += 1
-            return (3, bit_string, bit_len) if bit == '0' else (4, bit_string, bit_len)  # 0 or 1 <- 1 <- 0
+            m_code = 3 if bit == '0' else 4
         else:
             bit, bit_string = read_and_pop(bit_string, 1)
             bit_len += 1
             if bit == '0':
                 bit, bit_string = read_and_pop(bit_string, 1)
                 bit_len += 1
-                return (5, bit_string, bit_len) if bit == '0' else (6, bit_string, bit_len)  # 0 or 1 <- 1 <- 0
+                m_code = 5 if bit == '0' else 6
             else:
                 bit, bit_string = read_and_pop(bit_string, 1)
                 bit_len += 1
                 if bit == '0':
-                    return 7, bit_string, bit_len
+                    m_code = 7
                 else:
                     bit, bit_string = read_and_pop(bit_string, 1)
                     bit_len += 1
                     if bit == '0':
-                        return 8, bit_string, bit_len
+                        m_code = 8
                     else:
                         bit, bit_string = read_and_pop(bit_string, 1)
                         bit_len += 1
                         if bit == '0':
-                            return 9, bit_string, bit_len
+                            m_code = 9
                         else:
                             bit, bit_string = read_and_pop(bit_string, 1)
                             bit_len += 1
                             if bit == '0':
                                 bit, bit_string = read_and_pop(bit_string, 1)
                                 bit_len += 1
-                                return (10, bit_string, bit_len) if bit == '0' else (11, bit_string, bit_len)
+                                m_code = 10 if bit == '0' else 11
                             else:
                                 bit, bit_string = read_and_pop(bit_string, 1)
                                 bit_len += 1
                                 if bit == '0':
                                     bit, bit_string = read_and_pop(bit_string, 1)
                                     bit_len += 1
-                                    return (12, bit_string, bit_len) if bit == '0' else (13, bit_string, bit_len)
+                                    m_code = 12 if bit == '0' else 13
                                 else:
                                     bit, bit_string = read_and_pop(bit_string, 1)
                                     bit_len += 1
-                                    return (14, bit_string, bit_len) if bit == '0' else (15, bit_string, bit_len)
-    
+                                    m_code = 14 if bit == '0' else 15
+    return m_code, bit_string, bit_len
 
 
 def huffman_3(bit_string):
@@ -99,10 +95,8 @@ def huffman_3(bit_string):
     if bit == '0':
         bit, bit_string = read_and_pop(bit_string, 1)
         bit_len += 1
-        if bit == '0':
-            return 0, bit_string, bit_len
-        else:
-            return 1, bit_string, bit_len
+        if bit == '1':
+            m_code = 1
     else:
         m_code = 2
         for i in range(k - 3):
@@ -112,7 +106,7 @@ def huffman_3(bit_string):
                 break
             else:
                 m_code += 1
-        return m_code, bit_string, bit_len
+    return m_code, bit_string, bit_len
 
 
 def huffman(bit_string: str, brc: int):
