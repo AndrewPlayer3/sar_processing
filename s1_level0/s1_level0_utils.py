86d50e1545b2f2f69eb031118731a48d9780a87e
Andrew Player <andrewplayer3@gmail.com>
2024-08-08 04:31:37 -0500

    much cleaner huffman decoding

--- s1_level0/s1_level0_utils.py
+++ s1_level0/s1_level0_utils.py
@@ -5,6 +5,8 @@ Description: Some basic utility functions necessary for decoding Level-0 data.
              https://sentinels.copernicus.eu/documents/247904/2142675/Sentinel-1-SAR-Space-Packet-Protocol-Data-Unit.pdf
 """
 
+from s1_level0_structs import BRC_TO_HUFFMAN_START_BIT_LEN, BRC_TO_HUFFMAN_CODING
+
 
 def create_bit_string(bytes_string: str):
     bit_string = ''
@@ -17,121 +19,21 @@ def read_and_pop(bit_string: str, bit_length: int):
     return bit_string[0:bit_length], bit_string[bit_length:]
 
 
-def bit_len_map(brc: int):
-    match brc:
-        case 0:
-            k = 4
-        case 1:
-            k = 5
-        case 2:
-            k = 7
-        case 3:
-            k = 10
-        case 4:
-            k = 16
-        case other:
-            raise ValueError(f'Invalid BRC Value Encountered in `bit_len_map`: {brc}')
-    return k
-
-
-# Avert your eyes.
-def huffman_4(bit_string):
-    k = bit_len_map(4)
-    m_code = 0
-    bit, bit_string = read_and_pop(bit_string, 1)
-    bit_len = 1
-    if bit == '0':
-        bit, bit_string = read_and_pop(bit_string, 1)
-        bit_len += 1
-        if bit == '1':
-            m_code = 1 if bit == '0' else 2
-    else:
-        bit, bit_string = read_and_pop(bit_string, 1)
-        bit_len += 1
-        if bit == '0':
-            bit, bit_string = read_and_pop(bit_string, 1)
+def huffman_(bit_string, start_bit_len, bit_pattern_to_code):
+    bits = ''
+    bit_len = start_bit_len
+    temp_bit_string = ''
+    while bits not in bit_pattern_to_code:
+        bit_pattern, temp_bit_string = read_and_pop(bit_string, bit_len)
+        bits = bit_pattern
+        if bits not in bit_pattern_to_code:
             bit_len += 1
-            m_code = 3 if bit == '0' else 4
-        else:
-            bit, bit_string = read_and_pop(bit_string, 1)
-            bit_len += 1
-            if bit == '0':
-                bit, bit_string = read_and_pop(bit_string, 1)
-                bit_len += 1
-                m_code = 5 if bit == '0' else 6
-            else:
-                bit, bit_string = read_and_pop(bit_string, 1)
-                bit_len += 1
-                if bit == '0':
-                    m_code = 7
-                else:
-                    bit, bit_string = read_and_pop(bit_string, 1)
-                    bit_len += 1
-                    if bit == '0':
-                        m_code = 8
-                    else:
-                        bit, bit_string = read_and_pop(bit_string, 1)
-                        bit_len += 1
-                        if bit == '0':
-                            m_code = 9
-                        else:
-                            bit, bit_string = read_and_pop(bit_string, 1)
-                            bit_len += 1
-                            if bit == '0':
-                                bit, bit_string = read_and_pop(bit_string, 1)
-                                bit_len += 1
-                                m_code = 10 if bit == '0' else 11
-                            else:
-                                bit, bit_string = read_and_pop(bit_string, 1)
-                                bit_len += 1
-                                if bit == '0':
-                                    bit, bit_string = read_and_pop(bit_string, 1)
-                                    bit_len += 1
-                                    m_code = 12 if bit == '0' else 13
-                                else:
-                                    bit, bit_string = read_and_pop(bit_string, 1)
-                                    bit_len += 1
-                                    m_code = 14 if bit == '0' else 15
-    return m_code, bit_string, bit_len
-
-
-def huffman_3(bit_string):
-    k = bit_len_map(3)
-    m_code = 0
-    bit, bit_string = read_and_pop(bit_string, 1)
-    bit_len = 1
-    if bit == '0':
-        bit, bit_string = read_and_pop(bit_string, 1)
-        bit_len += 1
-        if bit == '1':
-            m_code = 1
-    else:
-        m_code = 2
-        for i in range(k - 3):
-            bit, bit_string = read_and_pop(bit_string, 1)
-            bit_len += 1
-            if bit == '0':
-                break
-            else:
-                m_code += 1
-    return m_code, bit_string, bit_len
+    return bit_pattern_to_code[bits], temp_bit_string, bit_len
 
 
 def huffman(bit_string: str, brc: int):
-    if brc == 3:
-        return huffman_3(bit_string)
-    elif brc == 4:
-        return huffman_4(bit_string)
-    else:
-        bit_len = 0
-        k = bit_len_map(brc)
-        m_code = 0
-        for i in range(k - 1):
-            bit, bit_string = read_and_pop(bit_string, 1)
-            bit_len += 1
-            if bit == '0':
-                break
-            else:
-                m_code += 1
-        return m_code, bit_string, bit_len
-
+    return huffman_(
+        bit_string,
+        BRC_TO_HUFFMAN_START_BIT_LEN[brc],
+        BRC_TO_HUFFMAN_CODING[brc]
+    )
