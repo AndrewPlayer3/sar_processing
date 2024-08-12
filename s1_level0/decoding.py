ae8ae66fe33cb801a03af412b59b99373e14904e
Andrew Player <andrewplayer3@gmail.com>
2024-08-12 00:17:19 -0500

    annotation and index file decoding

--- s1_level0/decoding.py
+++ s1_level0/decoding.py
@@ -81,9 +81,78 @@ def build_data_word_dict(PacketGenerator, num_packets, log: bool = True, log_int
 
         key, pos = SUB_COMM_KEY_POS[sc_data_word_index]
         pos = pos * WORD_SIZE
-        sub_comm_dict[key] = sub_comm_dict[key][0:pos] + packet.secondary_header()['sc_data_word'] + sub_comm_dict[key][pos+WORD_SIZE:]
+        data_word =  packet.secondary_header()['sc_data_word']
+        sub_comm_dict[key] = sub_comm_dict[key][0:pos] + data_word + sub_comm_dict[key][pos+WORD_SIZE:]
     
     if sc_data_word_index != initial_data_word_index:
         data_word_dicts.append(sub_comm_dict)
 
     return data_word_dicts
+
+
+def index_decoder(raw_data):
+    """
+    Block Index: 36 Octets
+        Date/Time: 8 Octets (MJD 1950)
+        Data Time Delta: 8 Octets (milliseconds)
+        Size of Data Unit: 4 Octets
+        Data Unit Offset from File Beginning: 4 Octets (1 indexed)
+        Bytes Offset from File Beginning: 8 Octets (0 indexed)
+        Variable Data Size Flag: 1 Octet
+        Spare Data to Align to 36 Bytes: 3 Octets
+    """
+    index_records = []
+    while raw_data:
+        data = raw_data.read(36)
+        if not data:
+            break
+        record = {
+            'date_time': int.from_bytes(data[0:8], 'big'),
+            'time_delta': int.from_bytes(data[8:16], 'big'),
+            'data_size': int.from_bytes(data[16:20], 'big'),
+            'unit_offset': int.from_bytes(data[20:24], 'big'),
+            'byte_offset': int.from_bytes(data[24:32], 'big'),
+            'variable_flag': int.from_bytes(data[32:33], 'big'),
+            'spare_data': int.from_bytes(data[33:36], 'big'),
+        }
+        index_records.append(record)
+    return index_records
+
+
+def annotation_decoder(raw_data):
+    """
+    Annotation Record: 26 Octets
+        Aquisition Time: 2 Octets (utc time, days since Jan 1. 2000)
+        Aquisition Time: 4 Octets (utc time, milliseconds since beginning of day)
+        Aquisition Time: 2 Octets (utc time, microseconds since the last millisecond)
+        Packet Length: 2 Octets
+        Number of Transfer Frames: 2 Octets
+        CRC Error Flag: 1 Octet
+        Bit Type: 1 Bit
+        Bit 2 Type: 2 Bits (2-bit bit mask)
+        Bit 6 Type: 6 Bits (6-bit bit mask)
+        Spare Field: 1 Octet
+    """
+    annotation_records = []
+    while raw_data:
+        data = raw_data.read(26)
+        if not data:
+            break
+        bit_string = create_bit_string(data)
+        record = {
+            'days': int(bit_string[0:16], 2),
+            'milliseconds': int(bit_string[16:48], 2),
+            'microseconds': int(bit_string[48:64], 2),
+            'days': int(bit_string[64:80], 2),
+            'milliseconds': int(bit_string[80:112], 2),
+            'microseconds': int(bit_string[112:128], 2),
+            'packet_length': int(bit_string[128:144], 2),
+            'num_transfer_frames': int(bit_string[144:160], 2),
+            'error_flag': int(bit_string[160:168], 2),
+            'bit_1_type': int(bit_string[168:169], 2),
+            'bit_2_type': int(bit_string[169:171], 2),
+            'bit_6_type': int(bit_string[171:177], 2),
+            'spare_field': int(bit_string[177:185], 2)
+        }
+        annotation_records.append(record)
+    return annotation_records
