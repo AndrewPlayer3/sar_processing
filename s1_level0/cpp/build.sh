4d902884b17e2106c0a38224f1966256cbd5bc6e
Andrew Player <andrewplayer3@gmail.com>
2024-08-17 14:37:55 -0500

    pick clang++ or g++ based on os

--- s1_level0/cpp/build.sh
+++ s1_level0/cpp/build.sh
@@ -2,4 +2,10 @@
 
 echo "Compiling main to bin/main."
 
-g++ -std=c++20 -O3 main.cpp packet.cpp packet_decoding.cpp decoding_utils.cpp -o bin/main
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+	g++ -fopenmp -std=c++20 -O3 main.cpp packet.cpp packet_decoding.cpp decoding_utils.cpp -o bin/main
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+	/opt/homebrew/opt/llvm/bin/clang++ -fopenmp -std=c++20 -O3 main.cpp packet.cpp packet_decoding.cpp decoding_utils.cpp -o bin/main
+else
+	echo "$OSTYPE is not supported by this script, see the commands for what needs to be compiled."
+fi
